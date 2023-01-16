0df693a85aae602b003290d50d741dcf855ed8e7
Bayo <jbayo@iesebre.com>
2023-01-16 15:40:59 +0100

    URL Downloader

--- Directoris1.py
+++ Directoris1.py
@@ -1,123 +1,134 @@
+import os.path
 import shutil
-from getpass import getuser
-import os
 import urllib.request
+from getpass import getuser
 
-usuari = getuser()
-#Mirem si l'usuari esta creat accedic al directori, mes un getuser
-DirectoriUrlGeneral = "/home/" + usuari + "/URLDowloaderBayo"
-#Amb el os.path.exists comprovem si el didrectori esta creat, si no ho esta el creem
-existeixDirecotriUrlGeneral = os.path.exists(DirectoriUrlGeneral)
-if not (existeixDirecotriUrlGeneral):
-    os.mkdir(DirectoriUrlGeneral)
-    print("S'ha creat un directori nou per treballar\n")
-else:
-    print("El direcotri amb el que treballaras ja el tens creat\n")
-while (True):
-    #Introduim lindex amb les diferentes opcions
-    print("1. Sortir del programa")
-    print("2. Descarregar URL")
-    print("3. Actualitzar URL")
-    resposta = int(input("Indtrudueix una opció: "))
 
-    if(resposta == 1):
-        #Soritm del programa amb un break
-        print("Andusiau!")
+def actualitzarFitxer():
+    urlActualitzar = input("Posa la URL a actualitzar: ")
+    Fitxer = comprovarPathDomini(urlActualitzar) + "/" + finalPathFitxer(urlActualitzar) + ".html"
+
+    if os.path.exists(Fitxer):
+        # BORREM LA CARPETA
+        shutil.rmtree(comprovarPathDomini(urlActualitzar))
+        # GENEREM LA CARPETA
+        generarCarpeta(urlActualitzar)
+        # CREEM EL FITXER
+        generarFitxer(urlActualitzar)
+        print("URL actualitzada correctament")
+
+    else:
+        print("Aquesta URL no existeix i per tant no es pot actualitzar")
+
+
+def descarregarURL():
+    urlDescarregar = input("Posa la URL a descarregar: ")
+    # MIREM SI EL FITXER EXISTEIX
+
+    if existeixFitxer(urlDescarregar):
+#COPIAR FITXER
+        fitxerActual = open(comprovarPathDomini(urlDescarregar) + "/" + finalPathFitxer(urlDescarregar) + ".html")
+        fitxerNou = open(comprovarPathDomini(urlDescarregar) + "/" + finalPathFitxer(urlDescarregar) + "-OLD-" + valorOLD(urlDescarregar) + ".html", "w")
+#ESCIRURE EL FITXER
+        fitxerNou.write(fitxerActual.read())
+        fitxerActual.close()
+        fitxerNou.close()
+#BORRAR FITXER ANTIC
+        os.remove(comprovarPathDomini(urlDescarregar) + "/" + finalPathFitxer(urlDescarregar) + ".html")
+
+        print("El fitxer '" + finalPathFitxer(urlDescarregar) + ".html' s'ha actualitzat amb el nom: '" + finalPathFitxer(urlDescarregar) + "-OLD-" + valorOLD(urlDescarregar) + ".html")
+        generarFitxer(urlDescarregar)
+        print("S'ha creat el fitxer '" + finalPathFitxer(urlDescarregar) + ".html'")
+        print()
+# SI EL FITXER NO EXISTEIX
+    else:
+#CREAR CARPETA
+        generarCarpeta(urlDescarregar)
+        print("Aquest fitxer no existeix, per tant generem una nova carpeta '" + urlDescarregar.split("/")[2]+"'")
+
+        generarFitxer(urlDescarregar)
+        print("I també generem el fitxer: '" + finalPathFitxer(urlDescarregar) + ".html'")
+
+
+def existeixFitxer(url):
+    pathFitxer = comprovarPathDomini(url) + "/" + finalPathFitxer(url) + ".html"
+    # RETORNEM SI EL PATH EXISTEIX
+    return os.path.exists(pathFitxer)
+
+
+def comprovarPathDomini(url):
+    global dominiMeu
+    carpetaSeparada = url.split("/")
+    #COMPROVEM SI ES UNA URL
+    try:
+        dominiMeu = carpetaSeparada[2]
+    except:
+        print("Aquesta URL no és correcta")
+
+    path = "/home/" + getuser() + "/URLDownloaderJoanBayo/" + dominiMeu
+    return path
+
+
+def finalPathFitxer(url):
+#DEFINIM EL PATH FINAL DEL FITXER
+    finalPathFitxer = url.split("/")
+    numeroFinalPath = (len(finalPathFitxer) -1)
+    finalPathFitxer = finalPathFitxer[numeroFinalPath]
+    return (finalPathFitxer)
+
+
+
+def valorOLD(url):
+#COMPROVEM ELS ALTRES FITXERS PER MIRAR EL VALOR ASSIGNAR
+    valorOLD = 1
+    while True:
+        FitxerOLD = comprovarPathDomini(url) + "/" + finalPathFitxer(url) + "-OLD-" + str(valorOLD) + ".html"
+        if not os.path.exists(FitxerOLD):
+            return str(valorOLD)
+        valorOLD += 1
+
+
+def generarFitxer(url):
+    Fitxer = comprovarPathDomini(url) + "/" + finalPathFitxer(url) + ".html"
+    connexio = urllib.request.urlopen(url)
+    contingut = connexio.read().decode('UTF-8')
+
+    if not existeixFitxer(url):
+        WriteFitxer = open(Fitxer, "w")
+        WriteFitxer.write(contingut)
+        WriteFitxer.close()
+
+
+def generarCarpeta(url):
+    CarpetaPrograma = "/home/" + getuser() + "/URLDownloaderJoanBayo"
+    existeixCarpetaPrograma = os.path.exists(CarpetaPrograma)
+
+    # CREEM LA CARPETA SI NO EXISTEIX
+    if not existeixCarpetaPrograma:
+        os.mkdir(CarpetaPrograma)
+
+    # DEFINIM EL PATH DE LA CARPETA DEL DOMINI
+    CarpetaDomini = comprovarPathDomini(url)
+    existeixCarpetaDomini = os.path.exists(CarpetaDomini)
+
+    # CREEM LA CARPETA SI NO EXISTEIX
+    if not existeixCarpetaDomini:
+        os.mkdir(CarpetaDomini)
+
+
+while True:
+    print("URL DOWNLOADER")
+    print("1- Sortir del programa")
+    print("2- Descarregar URL")
+    print("3- Actualitzar URL")
+    opcio = int(input("Que vols fer: "))
+
+    if opcio == 1:
+        print("Andusiauuu!")
         break
 
-    if (resposta == 2):
+    if opcio == 2:
+        descarregarURL()
 
-        url = input("Introduiex la URL per descargar: ")
-        #Agafem lenllaç i amb el split, aconseguim separar-lo per les barres (/)
-        nomUrlSeparada = url.split("/")
-        #Creem el direcotri en el qual es guardarar el fitxer, ho fem utilitzan el direcotir creat anteriorment+la segona part de la url separada
-        DirectoriUrl= DirectoriUrlGeneral + "/" + nomUrlSeparada[2]
-        #fem el mateix que hem fet amb el direcotir anterior, comprovem si existeix i sino el creem
-        existeixDirectoriUrl = os.path.exists(DirectoriUrl)
-        if not (existeixDirectoriUrl):
-            os.mkdir(DirectoriUrl)
-        #Ara llegim el link anterior, per accedir al seu HTML i posteriroment printejarlo en un fitxer, amb el nom de la penultima part de la url
-        #Agafa el link
-        connexio = urllib.request.urlopen(url)
-        #EL lletgim amb HTML
-        contingut = connexio.read().decode('UTF-8')
-        #Creem el nou fitxer amb el nom de la carpeta anterior mes la penultima pert, per aixo fiquem (-1)
-        nomFile = DirectoriUrl + "/" + nomUrlSeparada[len(nomUrlSeparada) - 1]
-        #comprovem si el fitxer que volem fer ja existeix
-        fitxerFinal = nomUrlSeparada[len(nomUrlSeparada)-1].split(".")
-        existixFitxer = os.path.exists(nomFile)
-        fitxerRenombrat = DirectoriUrl + "/" + fitxerFinal[0] + "OLD-" + str (variableOLD) + "." + fitxerFinal[len(fitxerFinal) -1]
-        if (existixFitxer):
-            variableOLD = int()
-            while (True):
-                try:
-                    open(nomFile,"x")
-                except FileExistsError:
-                    variableOLD += 1
-                    while os.path.exists(fitxerRenombrat):
-
-                        try:
-                            open(DirectoriUrl + "/" + nomUrlSeparada[len(nomUrlSeparada)-1],"x")
-
-                        except FileExistsError:
-                            break
-
-                    else:
-                        break
-                        shutil.copyfile(nomFile, fitxerRenombrat)
-
-            if not os.path.exists(fitxerRenombrat):
-                f1 = open(fitxerRenombrat, "w")
-
-            shutil.copyfile(nomFile, fitxerRenombrat)
-
-        if not (existixFitxer):
-            f2 = open(nomFile,"w")
-            f2 = write(contingut)
-
-
-        print("fitxer creat")
-
-
-
-    if (resposta == 3):
-        print("Quina URL vols actualitzar?")
-        urlActualitzada = input()
-        urlSeparada = urlActualitzada.split("/")
-        urlSeparadaFinal = urlSeparada[len(urlSeparada)-1].split(".")
-        connexio = urllib.request.urlopen(link)
-        contingut = connexio.read().decode("UTF-8")
-        if not os.path.exists(nomFile):
-            print("Aquest enllaç no esta guardat, i per tan no pot ser modificat")
-        else:
-            variableOLD = int()
-            while (True):
-                try:
-                    os.remove(nomFile)
-                    variableOLD += 1
-
-                    while os.path.exists(fitxerRenombrat):
-                        os.remove(fitxerRenombrat)
-                        variableOLD += 1
-
-                except Exception:
-                    break
-                else:
-                    break
-
-    if not os.path.exists(nomFile):
-        f1 = open(nomFile, "w")
-        f1.write(contingut)
-
-
-
-
-
-
-
-
-
-
-
-
-
+    if opcio == 3:
+        actualitzarFitxer()
